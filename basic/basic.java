57bde6a156b73d6e0298548f5210d69e8aafc9a8
Shubham kahar <Shubhamkahar196@gmail.com>
2025-07-11 23:20:12 +0530

    basic

---
+++ basic/basic.java
@@ -0,0 +1,5 @@
+public class basic {
+    public static void main(String[] args) { // psvm shortcut  //boilerplate code
+        System.out.println("my first program in java langauge "); // syout shortcut
+    }
+}
